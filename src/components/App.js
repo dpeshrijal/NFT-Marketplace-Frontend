cf6ac36a71d2fefdef49a13da530754fb65aa3b0
Deepesh Rijal <deepeshrijal@gmail.com>
2022-06-24 13:37:38 +0545

    functions to connect metamask and to load contract

---
+++ src/components/App.js
@@ -0,0 +1,82 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NFTAbi from "../utils/NFT.json";
+import NFTMarketplaceAbi from "../utils/NFTMarketplace.json";
+import { useState } from "react";
+import { ethers } from "ethers";
+import Navbar from './Navbar';
+import Home from './Home.js';
+import Create from './Create.js';
+import MyListedItems from './MyListedItems.js';
+import MyPurchases from './MyPurchases.js';
+
+import './App.css';
+
+function App() {
+    const [account, setAccount] = useState("");
+    const [nft, setNFT] = useState("");
+    const [marketplace, setMarketplace] = useState();
+
+    const nftAddress = "0x9e599631D8af4D39D8E64e344bEd2bD4b2104B4C";
+    const nftMarketplaceAddress = "0xD75cBfF07470F17F469860eAD501aCa84C4E0B13";
+
+    const web3Handler = async () => {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0])
+        // Get provider from Metamask
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        // Set signer
+        const signer = provider.getSigner()
+
+        window.ethereum.on('chainChanged', (chainId) => {
+            window.location.reload();
+        })
+
+        window.ethereum.on('accountsChanged', async function (accounts) {
+            setAccount(accounts[0])
+            await web3Handler()
+        })
+        loadContracts(signer)
+    }
+    const loadContracts = async (signer) => {
+        // Get deployed copies of contracts
+
+        setMarketplace(new ethers.Contract(nftMarketplaceAddress, NFTMarketplaceAbi.abi, signer));
+        setNFT(new ethers.Contract(nftAddress, NFTAbi.abi, signer));
+    }
+
+    return (
+
+        <BrowserRouter>
+            <div className="App">
+                <Navbar web3Handler={web3Handler} account={account} />
+
+                <div>
+
+                    <Routes>
+                        <Route path="/" element={
+                            <Home marketplace={marketplace} nft={nft} />
+                        } />
+                        <Route path="/create" element={
+                            <Create marketplace={marketplace} nft={nft} />
+                        } />
+                        <Route path="/my-listed-items" element={
+                            <MyListedItems marketplace={marketplace} nft={nft} account={account} />
+                        } />
+                        <Route path="/my-purchases" element={
+                            <MyPurchases marketplace={marketplace} nft={nft} account={account} />
+                        } />
+                    </Routes>
+
+                </div>
+            </div>
+        </BrowserRouter>
+
+    )
+
+}
+
+
+
+
+
+export default App;
